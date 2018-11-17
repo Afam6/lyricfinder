276a8c97a3126908449310650c2de6e8e181a718
Afam <afamezechukwu@gmail.com>
2018-11-17 15:06:09 +0100

    Updates

---
+++ precache-manifest.8573f639fd00b23453c81331bc9a09bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "404cd28e19c4c9466cf3",
+    "url": "/lyricfinder/static/js/runtime~main.404cd28e.js"
+  },
+  {
+    "revision": "2ea945ffcc90a936bf00",
+    "url": "/lyricfinder/static/js/main.2ea945ff.chunk.js"
+  },
+  {
+    "revision": "3cacb80bffe45e3892e9",
+    "url": "/lyricfinder/static/js/1.3cacb80b.chunk.js"
+  },
+  {
+    "revision": "2ea945ffcc90a936bf00",
+    "url": "/lyricfinder/static/css/main.9743dcc1.chunk.css"
+  },
+  {
+    "revision": "e7617c3c2162712c116f4db4459c17a6",
+    "url": "/lyricfinder/index.html"
+  }
+];
